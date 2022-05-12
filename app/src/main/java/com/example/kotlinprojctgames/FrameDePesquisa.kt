2296d6759351b4af577ad5fddb38943f8d9f0bc1
ezequielmathias32@gmail.com <ezequielmathias32@gmail.com>
2022-05-12 16:44:18 -0300

    falta alguns detalhes

---
+++ app/src/main/java/com/example/kotlinprojctgames/FrameDePesquisa.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinprojctgames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FrameDePesquisa : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_frame_de_pesquisa)
+    }
+}
---
+++ app/src/main/java/com/example/kotlinprojctgames/UltimaFrame.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinprojctgames
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UltimaFrame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ultima_frame)
+    }
+}
